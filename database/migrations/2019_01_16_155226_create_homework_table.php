25578906c9a15297f222dc42d73962e6c7ea0fb6
Diego Steiner <diego.steiner@u041.ch>
2019-01-16 16:56:03 +0100

    Add Homework migration

---
+++ database/migrations/2019_01_16_155226_create_homework_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHomeworkTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('homeworks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('subject');
+            $table->string('task');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('homeworks');
+    }
+}
