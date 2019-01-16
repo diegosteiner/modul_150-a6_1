76378d312fc6f2cbe2be25899ca42f12645bc605
Diego Steiner <diego.steiner@u041.ch>
2019-01-16 17:04:34 +0100

    Add homework routes

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,29 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+/**
+ * Display All Tasks
+ */
+Route::get('/homework', function () {
+    return view('homework');
+});
+
+/**
+ * Add A New Task
+ */
+Route::post('/homework', function (Request $request) {
+    //
+});
+
+/**
+ * Delete An Existing Task
+ */
+Route::delete('/homework/{id}', function ($id) {
+    //
 });
