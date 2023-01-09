8184be35c7e564245e8b62a1692acead38f42057
Diego Steiner <diego.steiner@u041.ch>
2023-01-09 13:16:33 +0100

    feature: add controller

---
+++ app/Models/Homework.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Homework extends Model
+{
+    public function subject() {
+        return $this->belongsTo(Subject::class)->withDefault();
+    }
+}
